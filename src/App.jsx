c095bf25f90501f1198450e82c4766cd1d4b2aaf
jorge2101719 <rusosky01@gmail.com>
2023-09-01 00:28:49 -0400

    primera entrega: creada estructura básica

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, useRef } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Elemento App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
