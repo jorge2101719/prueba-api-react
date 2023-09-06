be4bd9e10c853aae324815cd18d2fcf04495e4dc
jorge2101719 <rusosky01@gmail.com>
2023-09-06 15:35:56 -0300

    pequeño cambio de texto

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <div className='contenedor-principal bg-primary p-1'>
-        <h1 className='my-3'>Índices económicos</h1>
+        <h1 className='my-3'>Indicadores económicos</h1>
 
         <div className='componentes bg-secondary'>
           <div className='miapi bg-info m-1'>
